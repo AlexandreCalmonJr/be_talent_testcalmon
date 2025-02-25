e666ef8c010121960c25935f02ce1858603ab404
AlexandreCalmonJr <87951095+AlexandreCalmonJr@users.noreply.github.com>
2025-02-25 19:41:51 -0300

    feat table ajust

--- lib/widgets/table_header.dart
+++ lib/widgets/table_header.dart
@@ -11,14 +11,46 @@ class TableHeader extends StatelessWidget {
       decoration: const BoxDecoration(
         color: Color(0xFFEDEFFB),
         borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(8), topRight: Radius.circular(8)),
+          topLeft: Radius.circular(8),
+          topRight: Radius.circular(8),
+        ),
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: const [
-          Expanded(flex: 1, child: Text("Foto", style: headerStyle)),
-          Expanded(flex: 3, child: Text("Nome", style: headerStyle)),
-          Icon(Icons.circle, color: Colors.black, size: 8,),
+        mainAxisAlignment: MainAxisAlignment.start, // Alinha do início
+        crossAxisAlignment: CrossAxisAlignment.center, // Centraliza verticalmente
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8), // Espaço entre "Foto" e "Nome"
+              child: Text(
+                "Foto",
+                style: headerStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8), // Ajuste o espaçamento conforme necessário
+              child: Text(
+                "Nome",
+                style: headerStyle,
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ),
+          Spacer(), // Adiciona um espaço flexível
+          Padding(
+            padding: const EdgeInsets.only(right
+            : 26), // Ajuste o espaçamento conforme necessário
+            child: Icon(
+              Icons.circle,
+              color: Colors.black,
+              size: 8,
+            ),
+          ),
         ],
       ),
     );
